1cc86309d54a19a6df1dddcda869306e88210109
s0lanum-tuber0sum <itirmkitchen@gmail.com>
2023-02-27 13:09:07 +0530

    test

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const webserver = express()
+ .use((req, res) =>
+   res.sendFile('/websocket-client.html', { root: __dirname })
+ )
+ .listen(3000, () => console.log(`Listening on ${3000}`))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const { WebSocketServer } = require('ws')
+const sockserver = new WebSocketServer({ port: 443 })
+sockserver.on('connection', ws => {
+ console.log('New client connected!')
+ ws.send('connection established')
+ ws.on('close', () => console.log('Client has disconnected!'))
+ ws.on('message', data => {
+   sockserver.clients.forEach(client => {
+     console.log(`distributing message: ${data}`)
+     client.send(`${data}`)
+   })
+ })
+ ws.onerror = function () {
+   console.log('websocket error')
+ }
 })
